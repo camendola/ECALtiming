7662d16a608de9a64468e5f510fa02d20dd56cc3
chiara amendola <chia.amendola@gmail.com>
2021-01-12 22:35:33 +0100

    buf fix in recal script

--- scripts/recalelf.py
+++ scripts/recalelf.py
@@ -22,19 +22,20 @@ if __name__ == '__main__':
 
     main_file = ROOT.TFile.Open(args.infile)
     main_tree = main_file.Get('selected')    
-    #main_tree.BuildIndex("runNumber", "eventNumber")
+    main_tree.BuildIndex("runNumber", "eventNumber")
     print("opened")
     recal_file = ROOT.TFile.Open(args.outfile, 'RECREATE')
-    recal_tree = main_tree.CloneTree(0)
+    recal_tree = main_tree.CloneTree(0) #ROOT.TTree('recal_tree', 'Recalibrated')
+    #main_tree.CloneTree(0)
     recal_tree.Print()
     print("cloned")
 
-    calib1 = array( 'd', [ 0 ] )
-    calib2 = array( 'd', [ 0 ] )
-    laser1 = array( 'd', [ 0 ] )
-    laser2 = array( 'd', [ 0 ] )
-    timeSeedSC1_recal = array( 'd', [ 0 ] )
-    timeSeedSC2_recal = array( 'd', [ 0 ] )
+    calib1 = array( 'f', [ 0 ] )
+    calib2 = array( 'f', [ 0 ] )
+    laser1 = array( 'f', [ 0 ] )
+    laser2 = array( 'f', [ 0 ] )
+    timeSeedSC1_recal = array( 'f', [ 0 ] )
+    timeSeedSC2_recal = array( 'f', [ 0 ] )
 
     recal_tree.Branch('calib1', calib1, 'calib1/f')
     recal_tree.Branch('calib2', calib2, 'calib2/f')
@@ -46,45 +47,49 @@ if __name__ == '__main__':
 
 
     path_calib = "/afs/cern.ch/work/c/camendol/CalibIOVs/ic-config.json"
-    #path_laser = "/afs/cern.ch/work/c/camendol/LaserIOVs/2018/"+args.era+"ic-config.json"
+    path_laser = "/afs/cern.ch/work/c/camendol/LaserIOVs/2018/"+args.era+"/ic-config.json"
     icman_calib = recal.ICManager(path_calib)    
-    #icman_laser = recal.ICManager(path_laser)    
+    icman_laser = recal.ICManager(path_laser)    
     
     print("got calib")
     i = 0
     
     for evt in main_tree:
-
-        calib1[0] = icman_calib.getIC(recal_tree.xSeedSC[0],
-                                   recal_tree.ySeedSC[0],
+        
+        calib1[0] = icman_calib.getIC(main_tree.xSeedSC[0],
+                                   main_tree.ySeedSC[0],
                                    0,
-                                   recal_tree.runNumber)
+                                   main_tree.runNumber)
 
-        calib1_first = icman_calib.getIC(recal_tree.xSeedSC[0],
-                                         recal_tree.ySeedSC[0],
+        calib1_first = icman_calib.getIC(main_tree.xSeedSC[0],
+                                         main_tree.ySeedSC[0],
                                          0,
                                          315252)
 
-        #laser1 = calib1_first - icman_laser.getIC(recal_tree.xSeedSC1,
-        #                          recal_tree.ySeedSC1,
-        #                          0,
-        #                          recal_tree.eventTime)
-        timeSeedSC1_recal[0] = recal_tree.timeSeedSC[0] - calib1[0] + laser1[0]
+        laser1[0] = calib1_first - icman_laser.getIC(main_tree.xSeedSC[0],
+                                                  main_tree.ySeedSC[0],
+                                                  0,
+                                                  main_tree.eventTime)
 
-        calib2[0] = icman_calib.getIC(recal_tree.xSeedSC[1],
-                                   recal_tree.ySeedSC[1],
+        timeSeedSC1_recal[0] = main_tree.timeSeedSC[0] - calib1[0] + laser1[0]
+
+        calib2[0] = icman_calib.getIC(main_tree.xSeedSC[1],
+                                   main_tree.ySeedSC[1],
                                    0,
-                                   recal_tree.runNumber)
-        calib2_first = icman_calib.getIC(recal_tree.xSeedSC[1],
-                                         recal_tree.ySeedSC[1],
+                                   main_tree.runNumber)
+        calib2_first = icman_calib.getIC(main_tree.xSeedSC[1],
+                                         main_tree.ySeedSC[1],
                                          0,
                                          315252)
-        #laser2 = calib2_first - icman_laser.getIC(recal_tree.xSeedSC2,
-        #                          recal_tree.ySeedSC2,
-        #                          0,
-        #                          recal_tree.eventTime)
-        timeSeedSC2_recal[0] = recal_tree.timeSeedSC[1] - calib2[0] + laser2[0]
+        laser2[0] = calib2_first - icman_laser.getIC(main_tree.xSeedSC[1],
+                                                  main_tree.ySeedSC[1],
+                                                  0,
+                                                  main_tree.eventTime)
 
+        timeSeedSC2_recal[0] = main_tree.timeSeedSC[1] - calib2[0] + laser2[0]
+
+        #if (calib1_first > 10 or  calib2_first > 10 or calib1[0]  > 10 or calib2[0]  > 10 or laser1[0]  > 10 or  laser2[0]  > 10 or timeSeedSC1_recal[0]  > 10 or timeSeedSC2_recal[0] > 10 ):
+        #    print ("===> ", calib1_first, calib2_first, calib1[0], calib2[0], laser1[0], laser2[0],timeSeedSC1_recal[0], timeSeedSC2_recal[0])
 
         recal_tree.Fill()
     
